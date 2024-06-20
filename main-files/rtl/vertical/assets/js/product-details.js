aa899ebb0d3f488f7eecf452fc83d21f157d4d28
bilpang28 <nabilacantik257@gmail.com>
2024-06-20 15:34:20 +0800

    first commit

---
+++ main-files/rtl/vertical/assets/js/product-details.js
@@ -0,0 +1,30 @@
+$(function() {
+	"use strict";
+
+
+    $('.product-gallery').owlCarousel({
+        loop:true,
+		rtl:true,
+        margin:10,
+        responsiveClass:true,
+        nav:false,
+        dots: false,
+        thumbs: true,
+        thumbsPrerendered: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+             }
+          }
+        })
+
+
+
+
+});
